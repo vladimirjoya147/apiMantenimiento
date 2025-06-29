33da58373090f4ec93c0dbf5741753cd6283f265
vladimir <vladimirjoya148@gmail.com>
2025-06-29 02:53:11 -0500

    agregando dominio pages

--- src/main/java/mantenimiento/Cibertec/Proyecto/Security/SecurityConfig.java
+++ src/main/java/mantenimiento/Cibertec/Proyecto/Security/SecurityConfig.java
@@ -36,7 +36,8 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(Arrays.asList(
                 "http://127.0.0.1:5500",
                 "http://localhost:5500",
-                "https://tudominio.com"
+                "https://tudominio.com",
+                "https://vladimirjoya147.github.io"
         ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
