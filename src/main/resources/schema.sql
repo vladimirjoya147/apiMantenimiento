fa02adb4441b8661d8210d6bda9eb3aeff49538c
vladimir <vladimirjoya148@gmail.com>
2025-06-28 22:21:19 -0500

    agregado schema sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,16 +1,15 @@
-
 CREATE TABLE usuario (
   id INT PRIMARY KEY AUTO_INCREMENT,
   nombre VARCHAR(100),
   correo VARCHAR(100),
   contrasena VARCHAR(100),
-  rol VARCHAR(50) 
+  rol VARCHAR(50)
 );
 
 CREATE TABLE equipo (
   id INT PRIMARY KEY AUTO_INCREMENT,
   nombre VARCHAR(100),
-  tipo VARCHAR(50), 
+  tipo VARCHAR(50),
   descripcion TEXT,
   sku varchar(50) UNIQUE NOT NULL,
   sede VARCHAR(100)
@@ -25,6 +24,6 @@ CREATE TABLE reporte_mantenimiento (
   tipo_mantenimiento VARCHAR(50),
   estado VARCHAR(50),
   sincronizado BOOLEAN DEFAULT FALSE,
-  FOREIGN KEY (equipo_id) REFERENCES Equipo(id),
-  FOREIGN KEY (tecnico_id) REFERENCES Usuario(id)
+  FOREIGN KEY (equipo_id) REFERENCES equipo(id),
+  FOREIGN KEY (tecnico_id) REFERENCES usuario(id)
 );
